53fb23bfaa2fc16be945a5d138cb79f6f8cda623
GeonNamGung <rndrjs123@naver.com>
2021-07-14 19:38:26 +0900

    모바일 네비게이션 스타일 변경

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./Navigation.module.css";
 import { Link, NavLink } from "react-router-dom";
-import { useTheme } from "@material-ui/core";
+import { IconButton, useTheme } from "@material-ui/core";
 import logo from "../../resources/logo.png";
+import classNames from "classnames";
+import SearchIcon from "@material-ui/icons/Search";
+import CompareIcon from "@material-ui/icons/Compare";
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
+import InfoIcon from "@material-ui/icons/Info";
 
 const Navigation = () => {
   const theme = useTheme();
@@ -46,6 +51,36 @@ const Navigation = () => {
         <li>
           <NavLink to={"/info"} activeStyle={{ textDecoration: "underline" }}>
             서비스 정보
+          </NavLink>
+        </li>
+      </ul>
+      <ul className={classNames(styles.bottom, styles.nav)}>
+        <li>
+          <NavLink exact to={"/"} activeClassName={styles.active}>
+            <IconButton>
+              <SearchIcon />
+            </IconButton>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/compare"} activeClassName={styles.active}>
+            <IconButton>
+              <CompareIcon />
+            </IconButton>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/report"} activeClassName={styles.active}>
+            <IconButton>
+              <ChatBubbleIcon />
+            </IconButton>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/info"} activeClassName={styles.active}>
+            <IconButton>
+              <InfoIcon />
+            </IconButton>
           </NavLink>
         </li>
       </ul>
