1f06014d5b5ac04ba21c116a4af32a983795f767
GeonNamGung <rndrjs123@naver.com>
2021-07-17 20:34:48 +0900

    로딩 스피너 크기 지정

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,8 +3,13 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 
 const Loading = () => {
   return (
-    <div className={"flex center"}>
-      <CircularProgress />
+    <div
+      className={"flex center"}
+      style={{
+        width: "100%"
+      }}
+    >
+      <CircularProgress size={64} />
     </div>
   );
 };
